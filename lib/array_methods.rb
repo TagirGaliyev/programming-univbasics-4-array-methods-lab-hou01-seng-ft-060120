ca82b85b3c1592be974be4648dc2b303ade411a9
Tagir Galiyev <w.tagir.galiyev@gmail.com>
2020-05-13 20:50:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,19 +7,17 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.size
 end
-
-puts using_sort([6,1,2,3,4,5])
